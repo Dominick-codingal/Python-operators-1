d494dcbc66b2ea95ef5211ba2b572dbcf43d8474
Dominick-codingal <dominickcodingal@gmail.com>
2024-12-12 03:42:06 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 75
+tree5 = 80
+tree6 = 160
+sum = tree1+tree2+tree3+tree4+tree5+tree6
+print("the sum of all the trees is: ", sum)
+average = sum/6
+print("the Average of all the trees is:", average)
